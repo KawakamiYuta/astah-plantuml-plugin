fc9808fb298815bce90ddec7b693d5153b36b3d4
KawakamiYuta <pnullq@gmail.com>
2022-01-23 13:24:40 +0900

    ステートマシン図からPlantUMLへの変換で、並行合成状態、選択疑似状態に対応

--- src/main/kotlin/com/change_vision/astah/plugins/converter/toplant/ToPlantStateDiagramConverter.kt
+++ src/main/kotlin/com/change_vision/astah/plugins/converter/toplant/ToPlantStateDiagramConverter.kt
@@ -4,32 +4,77 @@ import com.change_vision.jude.api.inf.model.*
 import com.change_vision.jude.api.inf.presentation.ILinkPresentation
 import com.change_vision.jude.api.inf.presentation.INodePresentation
 
+
 object ToPlantStateDiagramConverter {
-    fun convert(diagram: IStateMachineDiagram, sb: StringBuilder) {
+	var choce_num : Int = 0
+	// < vertex,getId(), choce_num>
+	var choice_list = mutableMapOf<String, Int>()
+	get() {
+		return field
+	}
+	set(value) {
+		field = value
+	}
+
+	fun convert(diagram: IStateMachineDiagram, sb: StringBuilder) {
         val rootVertexes =
             diagram.presentations
                 .filterIsInstance<INodePresentation>().map { it.model }
                 .filterIsInstance<IVertex>().filter { it.container !is IVertex }
-        rootVertexes.forEach { vertexConvert(it, sb, "") }
+
         val transitions = diagram.presentations
             .filterIsInstance<ILinkPresentation>().map { it.model }
             .filterIsInstance<ITransition>()
+
+        rootVertexes.forEach { vertexConvert(it, sb, "", transitions) }
+	/*
         transitions.forEach { transitionConvert(it, sb) }
+	*/
     }
 
 
-    private fun vertexConvert(vertex: IVertex, sb: StringBuilder, indent: String) {
+    private fun vertexConvert(vertex: IVertex, sb: StringBuilder, indent: String, transition: List<ITransition>) {
         when (vertex) {
             is IFinalState -> { /* Skip */
             }
             is IPseudostate -> { /* Skip */
+			if (vertex.isChoicePseudostate()) {
+				/*
+				if(choice_list.containsKey(vertex.getId())) {
+				}*/
+				sb.appendLine("${indent}state choice${choce_num} <<choice>>")
+				choice_list[vertex.getId()] = choce_num
+				choce_num += 1
+			}
             }
             is IState -> {
                 when {
                     vertex.subvertexes.isEmpty() -> sb.appendLine("${indent}state ${vertex.name}")
                     else -> {
-                        sb.appendLine("${indent}state ${vertex.name} {")
-                        vertex.subvertexes.forEach { vertexConvert(it, sb, "$indent  ") }
+                        sb.appendLine("${indent}state ${vertex.getFullName("_")} {")
+			println("### ${vertex.getRegionSize()}")
+			for(i in 0..vertex.getRegionSize()-1) {
+                        	vertex.getSubvertexes(i)?.forEach { vertexConvert(it, sb, "$indent  ", transition) }
+
+				// 領域内の遷移を出力
+				transition.forEach {
+					//if (vertex.getSubvertexes(i).contains(it.source) || vertex.getSubvertexes(i).contains(it.target)) {
+					//	sb.appendLine("${it.source} --> ${it.target}")
+					//	println("match ${it.source} , ${it.target}")
+					//}
+					//else {
+					//	println("nm ${vertex}, ${it.source} , ${it.target}")
+					//}
+					if (vertex.getSubvertexes(i).contains(it.source) || vertex.getSubvertexes(i).contains(it.target)) {
+						transitionConvert(it, sb)
+					}
+				}
+
+				// 領域の境界
+				if(i != vertex.getRegionSize()-1) {
+					sb.appendLine("--")
+				}
+			}
                         sb.appendLine("${indent}}")
                     }
                 }
@@ -39,8 +84,18 @@ object ToPlantStateDiagramConverter {
 
     private fun transitionConvert(transition: ITransition, sb: java.lang.StringBuilder) {
         sb.append(
+            //when (val source = transition.source) {
+            //val source = transition.source
             when (val source = transition.source) {
-                is IPseudostate -> "[*]"
+		    /*
+                is IPseudostate -> {
+			if (source.isInitialPseudostate()) "[*]" else "XXX" 
+		}
+		*/
+		is IPseudostate -> {
+			if (source.isInitialPseudostate()) "[*]" else 
+			if (source.isChoicePseudostate()) "choice${choice_list[source.getId()]}" else ""
+		}
                 is IFinalState -> "[*]"
                 is IState -> source.name
                 else -> "[*]"
@@ -49,7 +104,10 @@ object ToPlantStateDiagramConverter {
         sb.append(" --> ")
         sb.append(
             when (val target = transition.target) {
-                is IPseudostate -> "[*]"
+                is IPseudostate -> {
+			if (target.isInitialPseudostate()) "[*]" else
+			if (target.isChoicePseudostate()) "choice${choice_list[target.getId()]}" else ""
+		}
                 is IFinalState -> "[*]"
                 is IState -> target.name
                 else -> "[*]"
